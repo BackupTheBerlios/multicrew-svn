3e7effdddf75d49545c63c285f2df841d299e27d
schimmi <schimmi@cb9ff89a-abed-0310-8fc6-a4cabe7d48c9>
2005-01-19 00:49:17 +0000

    * cosmetic

--- multicrewcore/position.cpp
+++ multicrewcore/position.cpp
@@ -32,9 +32,6 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #define WAITTIME 100
 
 
-
-
-
 PositionModule::PositionModule( bool hostMode ) 
 	: MulticrewModule( "FSUIPCPos", hostMode ) {
 	MulticrewCore::multicrewCore()->registerModule( this );
